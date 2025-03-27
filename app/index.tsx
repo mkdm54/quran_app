16babd284df39f88c6ff65dbf7eb9dce600cb6a1
Makdum cinta C++ <makdumibrohim3@gmail.com>
2025-03-27 19:37:41 +0700

    tambahkan fitur loading dan daftar surah menggunakan FlatList

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,9 @@
+import { useState, useEffect } from "react";
 import { Text, View, StyleSheet, FlatList } from "react-native";
 import SurahCard from "@/components/SurahCard";
 import { listSurah } from "@/src/api/ListSurah";
 
-interface listSurah {
+interface Surah {
   nomor: number,
   nama: string,
   nama_latin: string,
@@ -14,9 +15,39 @@ interface listSurah {
 }
 
 export default function Index() {
+  const [dataSurah, setDataSurah] = useState<Surah[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fecthData = async () => {
+      setLoading(true);
+      const data = await listSurah()
+      if (data) {
+        setDataSurah(data);
+      }
+      setLoading(true);
+    }
+    fecthData();
+  }, []);
+
   return (
     <View style={styles.container}>
-
+      {loading ? (
+        <Text>Loading...</Text>
+      ) : (
+        <FlatList
+          data={dataSurah}
+          keyExtractor={(item) => item.nomor.toString()}
+          renderItem={({ item }) => (
+            <SurahCard
+              surah_number={item.nomor}
+              name={item.nama}
+              latin_name={item.nama_latin}
+              number_of_verses={item.jumlah_ayat}
+            />
+          )}
+        />
+      )}
     </View>
   );
 }
